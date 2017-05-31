1c9277284a4878379547082cadbea4fe9cb48c06
Simon <simon.carrignon@bsc.es>
2017-05-31 10:45:52 +0200

    few comment

--- splitTask.sh
+++ splitTask.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-max=$1
-taskfile=$2
-jobfile=$3
-outfold=$4
+max=$1 #number of subtask
+taskfile=$2 #original task file
+jobfile=$3  #original job file
+outfold=$4  #folder to put the temporary files (allows to run "parallel master" job)
 
 
 ntask=`wc -l < "$taskfile" `
@@ -23,6 +23,7 @@ do
 	if [ $prevjob_id -eq 0 ];
 	then
 		bsub  < $outfold/tmp.job_$i > $outfold/out
+		#this sed (and obviously the one in the else) could be avoided by using a -w with the jobname in stead of the jobid
 		prevjob_id=$(sed 's/^Job <\([0-9]\+\)>.*$/\1/' $outfold/out)
 		echo "First Job:  $prevjob_id"
 	else	
